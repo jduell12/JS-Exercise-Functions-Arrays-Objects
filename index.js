204a2cd2e7c18b07fc3945ffc1424456b13f4728
Jessica <duellje@oregonstate.edu>
2020-05-13 21:55:59 -0400

    completed C to F temp function

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ function addNumbers(num1, num2) {
  * the returned value should look like: 'Goodbye, Andy. Have a great day.'
  * 
 */
-function sayGoodbye(/* code here */) {
-  /* code here */
+function sayGoodbye(name) {
+  return "Goodbye, " + name + ". Have a great day.";
 }
 
 /**
@@ -54,9 +54,12 @@ function sayGoodbye(/* code here */) {
  * Hint 1: The formula for converting celsius to fahrenheit is t*9/5 + 32 where t is the temperature in celsius.
  * Hint 2: There is a very easy way to round numbers in JS. Do a google search to find out how. 
 */
-function temperatureCtoF(/* code here */) {
-  /* code here */
+function temperatureCtoF(celsiusTemp) {
+  let fTemp = celsiusTemp*9/5 + 32;
+  fTemp = Math.round(fTemp);
+  return fTemp;
 }
+
 
 /**
  * ### Challenge `temperatureInF`
@@ -181,7 +184,7 @@ var inventory = [
 */
 
 // Data from file 
-var data = require('data./inventory.js'); 
+//var data = require('data./inventory.js'); 
 
 /**
   * ### Example Array Challenge:
